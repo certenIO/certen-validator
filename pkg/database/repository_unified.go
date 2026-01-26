d00e521659bdef677686a895e916b119b13cc45f
Jason-Gregoire <jasonkgregoire@gmail.com>
2026-01-26 05:03:30 -0500

    Fix chain_execution_results INSERT missing placeholder $29

--- pkg/database/repository_unified.go
+++ pkg/database/repository_unified.go
@@ -571,7 +571,7 @@ func (r *UnifiedRepository) CreateChainExecutionResult(ctx context.Context, inpu
 			state_root, transactions_root, receipts_root,
 			raw_receipt, logs, platform_data,
 			observer_validator_id, workflow_step, anchor_id, submitted_at
-		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28)
+		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29)
 	`
 
 	var proofID interface{}
